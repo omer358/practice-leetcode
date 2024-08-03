e1ad6e477edf0f6ae9f2d048bb465c29aaa53dce
omer358 <omermaki358@gmail.com>
2024-08-03 22:57:45 +0300

    solving using map approach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import problems.easy.MakingTwoArraysEqualByReversingSubarrays
 fun main() {
     println(
         MakingTwoArraysEqualByReversingSubarrays
-            .canBeEqual(
+            .canBeEqualUsingMap(
                 intArrayOf(1, 3, 2), intArrayOf(1, 2, 3)
             )
     )
--- src/main/kotlin/problems/easy/MakingTwoArraysEqualByReversingSubarrays.kt
+++ src/main/kotlin/problems/easy/MakingTwoArraysEqualByReversingSubarrays.kt
@@ -11,4 +11,35 @@ object MakingTwoArraysEqualByReversingSubarrays {
         }
         return true
     }
+
+    fun canBeEqualUsingMap(target: IntArray, arr: IntArray): Boolean {
+        val targetMap = mutableMapOf<Int, Int>()
+        for (i in target) {
+            if (targetMap.containsKey(i)) {
+                targetMap[i] = targetMap.getOrDefault(i, 0) + 1
+            } else {
+                targetMap[i] = 1
+            }
+        }
+
+        val arrMap = mutableMapOf<Int, Int>()
+
+        for (i in arr) {
+            if (arrMap.containsKey(i)) {
+                arrMap[i] = arrMap.getOrDefault(i, 0) + 1
+            }else{
+                arrMap[i] = 1
+            }
+        }
+        println("arrMap: $arrMap")
+        if (arrMap.keys.size != arrMap.keys.size) {
+            return false
+        }
+        for (num in arrMap.keys) {
+            if (arrMap.getOrDefault(num, 0) != targetMap.getOrDefault(num, 0)) {
+                return false
+            }
+        }
+        return true
+    }
 }
